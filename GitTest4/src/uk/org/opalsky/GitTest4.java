606b4fe1e32164ff3427a2e158540796f3986cbf
Ken <ken@kjstewart.co.uk>
2018-07-10 19:59:35 +0100

    Init project

---
+++ GitTest4/src/uk/org/opalsky/GitTest4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.org.opalsky;
+
+/**
+ *
+ * @author Author
+ */
+public class GitTest4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.setVisible(true);
+            }
+        });
+    }
+
+}
